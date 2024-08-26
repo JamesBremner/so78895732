f5b34e39752a2cf824a0928d9ead2a8d42479294
JamesBremner <james@ravenspoint.com>
2024-08-26 15:39:48 -0400

    Code

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,148 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
+#include "graphTheory.h"
 
-class cGUI : public cStarterGUI
+std::string start;
+std::string end;
+int wildcardCountMax;
+int wildCardCount;
+std::vector<std::string> valid;
+raven::graph::sGraphData gd;
+
+int editDistance(
+    const std::string &s,
+    const std::string &e)
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+    int ed = 0;
+    for (int i = 0; i < s.length(); i++)
+        if (s[i] != e[i])
+            ed++;
+    return ed++;
+}
 
-        show();
-        run();
+bool isSane()
+{
+    int l = start.length();
+    if (end.length() != l)
+        return false;
+    return true;
+}
+
+bool Dijsktra()
+{
+    // find shortest path from start to end
+    gd.startName = start;
+    gd.endName = end;
+    auto path = raven::graph::path(gd);
+    if (path.second == -1)
+    {
+        std::cout << "no path available\n";
+        exit(1);
     }
 
-private:
-    wex::label &lb;
-};
+    // loop over path
+    int prev = -1;
+    for (int i = 0; i < path.first.size() - 1; i++)
+    {
+        // check if this step comes no closer to end
+        auto srcname = gd.g.userName(path.first[i]);
+        int src2end = editDistance(srcname, end);
+        if (src2end == prev)
+        {
+
+            wildCardCount++;
+            if (wildCardCount > wildcardCountMax)
+            {
+                std::cout << "ran out of wild cards\n";
+                exit(2);
+            }
+
+            // construct required wildcard
+            auto name1 = gd.g.userName(path.first[i - 1]);
+            auto name2 = gd.g.userName(path.first[i + 1]);
+            int firstdiff;
+            for (int c = 0; c < name1.size(); c++)
+                if (name1[c] != name2[c])
+                {
+                    firstdiff = c;
+                    break;
+                }
+            std::string wild = name1;
+            wild[firstdiff] = name2[firstdiff];
+            std::cout << " add wild " << wild << "\n";
+
+            // add wildcard
+            valid.insert(valid.begin(), wild);
+
+            // request restart
+            return false;
+        }
+        prev = src2end;
+        std::cout << srcname << "->";
+    }
+    std::cout << end << "\n";
+
+    return true;
+}
+
+void validGraph()
+{
+    gd.g.clear();
+    gd.edgeWeight.clear();
+    for (int i = 0; i < valid.size(); i++)
+        for (int j = i + 1; j < valid.size(); j++)
+        {
+            int ed = editDistance(valid[i], valid[j]);
+            if (ed > 1)
+                continue; // do not add links between words where edit distance > 1
+            gd.g.add(valid[i], valid[j]);
+            gd.edgeWeight.push_back(ed);
+            gd.edgeWeight.push_back(ed);
+        }
+}
+
+bool isPath()
+{
+    // check problem sanity
+    if (!isSane())
+        return false;
+
+    // check if we can make a path with the available wild cards
+    int ed = editDistance(start, end);
+    if (ed < wildcardCountMax)
+        return true;
+
+    // add wildcards until Dijsktra can find a path from start to end
+    wildCardCount = 0;
+    while (!Dijsktra())
+    {
+        validGraph();
+    }
+
+    return false;
+}
+
+void gen1()
+{
+    wildcardCountMax = 4;
+    start = "HEAD";
+    end = "TAIL";
+    valid = std::vector<std::string>{"HEAL", "TEAL", "TELL", "TALL"};
+    valid.push_back(start);
+    valid.push_back(end);
+}
 
 main()
 {
-    cGUI theGUI;
+    // generate sample problem
+    gen1();
+
+    // generate graph of valid words connected by edit distance of 1
+    validGraph();
+
+    // find ladder
+    isPath();
+
     return 0;
 }
