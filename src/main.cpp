d0233f42eb18efc4a7799e74b8597979616fdbe1
JamesBremner <james@ravenspoint.com>
2024-08-27 09:33:58 -0400

    wildcard method

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 #include <algorithm>
 #include "graphTheory.h"
 
-std::string start;
-std::string end;
 int wildcardCountMax;
 int wildCardCount;
 std::vector<std::string> valid;
@@ -26,17 +24,44 @@ int editDistance(
 
 bool isSane()
 {
-    int l = start.length();
-    if (end.length() != l)
+    int l = gd.startName.length();
+    if (gd.endName.length() != l)
         return false;
     return true;
+}
+
+void wildCard(
+    const raven::graph::path_cost_t &path,
+    int i)
+{
+    wildCardCount++;
+    if (wildCardCount > wildcardCountMax)
+    {
+        std::cout << "ran out of wild cards\n";
+        exit(2);
+    }
+
+    // construct required wildcard
+    auto name1 = gd.g.userName(path.first[i - 1]);
+    auto name2 = gd.g.userName(path.first[i + 1]);
+    int firstdiff;
+    for (int c = 0; c < name1.size(); c++)
+        if (name1[c] != name2[c])
+        {
+            firstdiff = c;
+            break;
+        }
+    std::string wild = name1;
+    wild[firstdiff] = name2[firstdiff];
+    std::cout << " add wild " << wild << "\n";
+
+    // add wildcard
+    valid.insert(valid.begin(), wild);
 }
 
 bool Dijsktra()
 {
     // find shortest path from start to end
-    gd.startName = start;
-    gd.endName = end;
     auto path = raven::graph::path(gd);
     if (path.second == -1)
     {
@@ -50,33 +75,11 @@ bool Dijsktra()
     {
         // check if this step comes no closer to end
         auto srcname = gd.g.userName(path.first[i]);
-        int src2end = editDistance(srcname, end);
+        int src2end = editDistance(srcname, gd.endName);
         if (src2end == prev)
         {
-
-            wildCardCount++;
-            if (wildCardCount > wildcardCountMax)
-            {
-                std::cout << "ran out of wild cards\n";
-                exit(2);
-            }
-
-            // construct required wildcard
-            auto name1 = gd.g.userName(path.first[i - 1]);
-            auto name2 = gd.g.userName(path.first[i + 1]);
-            int firstdiff;
-            for (int c = 0; c < name1.size(); c++)
-                if (name1[c] != name2[c])
-                {
-                    firstdiff = c;
-                    break;
-                }
-            std::string wild = name1;
-            wild[firstdiff] = name2[firstdiff];
-            std::cout << " add wild " << wild << "\n";
-
             // add wildcard
-            valid.insert(valid.begin(), wild);
+            wildCard(path, i);
 
             // request restart
             return false;
@@ -84,7 +87,7 @@ bool Dijsktra()
         prev = src2end;
         std::cout << srcname << "->";
     }
-    std::cout << end << "\n";
+    std::cout << gd.endName << "\n";
 
     return true;
 }
@@ -112,7 +115,7 @@ bool isPath()
         return false;
 
     // check if we can make a path with the available wild cards
-    int ed = editDistance(start, end);
+    int ed = editDistance(gd.startName, gd.endName);
     if (ed < wildcardCountMax)
         return true;
 
@@ -129,11 +132,11 @@ bool isPath()
 void gen1()
 {
     wildcardCountMax = 4;
-    start = "HEAD";
-    end = "TAIL";
+    gd.startName = "HEAD";
+    gd.endName = "TAIL";
     valid = std::vector<std::string>{"HEAL", "TEAL", "TELL", "TALL"};
-    valid.push_back(start);
-    valid.push_back(end);
+    valid.push_back(gd.startName);
+    valid.push_back(gd.endName);
 }
 
 main()
